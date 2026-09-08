2c889991ff6922852e0e77a2c7dbe64fc719b7aa
Aritro1603 <156192655+Aritro1603@users.noreply.github.com>
2026-09-08 15:15:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<AboutUs />} />
         <Route path="/plants" element={<ProductList />} />
